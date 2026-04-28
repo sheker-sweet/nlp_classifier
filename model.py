c4af96b15a8472bc4357fcdec08949e160ecee7f
Sheker Atayeva <sheker.atayeva2004@gmail.com>
2026-04-28 14:16:54 +0200

    added ml imports

--- model.py
+++ model.py
@@ -6,6 +6,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Machine Learning
+from sklearn.model_selection import train_test_split, TimeSeriesSplit
+from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.pipeline import Pipeline
+# from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix # add more evaluation tools for the nlp component
+
+
+
 # Sentiment analysis 
 import nltk
 nltk.download('vader_lexicon')
@@ -14,6 +25,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 analyzer = SentimentIntensityAnalyzer()
 
 
-import yfinance as yf
+# import yfinance as yf
+
 sp500 = yf.download("^GSPC", start="2021-09-30", end="2022-09-30")
 
